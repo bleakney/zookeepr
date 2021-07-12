7ea940d177415a7b3da5f22a1776c9351d041aeb
Bleakney Bissett <bleakneybissett@gmail.com>
2021-07-12 15:44:05 -0500

    something is broken

--- server.js
+++ server.js
@@ -16,11 +16,6 @@ function filterByQuery(query, animalsArray) {
         }
         // Loop through each trait in the personalityTraits array:
         personalityTraitsArray.forEach(trait => {
-
-        
-        // Check the trait against each animal in the filteredResults array.
-        // it is initially a copy of the animalsArray, but here we're updating it for each trait in the .forEach() loop
-        // For each trait being targeted by the filter, the filteredResults array will then contain only the entries that contain the trait, so at the end we'll have an array of animals that every one of the traits when the .forEach() loop is finished.
         filteredResults = filteredResults.filter(
             animal => animal.personalityTraits.indexOf(trait) !== -1
         );
@@ -37,6 +32,11 @@ function filterByQuery(query, animalsArray) {
     }
     // return the filtered results:
     return filteredResults;
+};
+
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
 }
 
 app.get('/api/animals', (req, res)  => {
@@ -45,6 +45,11 @@ app.get('/api/animals', (req, res)  => {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+
+app.get('/api.animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    res.json(result);
 });
 
 app.listen(PORT, () => {
